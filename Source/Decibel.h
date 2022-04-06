b819dc4d115d90e9207de9626fb1e3dde70e375f
Dr Entropy <rjljr2@gmail.com>
2022-04-06 15:28:41 -0700

    Removed some comments PFM11-7

--- Source/Decibel.h
+++ Source/Decibel.h
@@ -8,21 +8,6 @@
   ==============================================================================
 */
 
-/*
-Rule of 3
-math operators for when this object is on the left side of the +=, -=, etc
-math operators for when this object is on the right side of the =: auto v = gain1 + gain2;   These should be friend functions
-comparison operators.  these should be friend functions
- 
-FloatType getGain() const
-FloatType getDb() const
-setGain(FloatType g)
-setDb(FloatType db)
- 
-2 constructors:
-default constructor
-a constructor that takes a FloatType parameter to initialize your member variable.
-*/
 
 #pragma once
 #include <JuceHeader.h>
