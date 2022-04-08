d3185a9894399d20af4b841af94120350ec32d78
Dr Entropy <rjljr2@gmail.com>
2022-04-08 13:10:31 -0700

    Add class declaration PFM11-8
    Copy pasta from Jira

--- Source/Fifo.h
+++ Source/Fifo.h
@@ -9,3 +9,29 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+
+template<typename T, size_t Size>
+struct Fifo
+{
+    size_t getSize() const noexcept
+    {
+        return Size;
+    };
+    
+    //used when T is AudioBuffer<float>
+    void prepare(int numSamples, int numChannels);
+    
+    //used when T is std::vector<float>
+    void prepare(size_t numElements);
+    
+    bool push(const T& t);
+    bool pull(T& t);
+    
+    int getNumAvailableForReading() const;
+    int getAvailableSpace() const;
+private:
+    juce::AbstractFifo fifo { Size };
+    std::array<T, Size> buffer;
+};
