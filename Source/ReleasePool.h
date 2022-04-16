55083cb1a866036f2606a4f147676b346e2232ce
Dr Entropy <rjljr2@gmail.com>
2022-04-16 11:02:35 -0700

    Complete but untested ReleasePool PFM11-11

--- Source/ReleasePool.h
+++ Source/ReleasePool.h
@@ -10,28 +10,55 @@
 
 #pragma once
 #include <JuceHeader.h>
+#include "Fifo.h"
 
-template <typename ObjectType>
-struct ReleasePool : Timer {
+template <typename ObjectType, size_t PoolSize>
+struct ReleasePool : juce::Timer {
     using Ptr = ReferenceCountedObjectPtr<ObjType>
     using Array = RefferenceCountedArray<ObjType>
-    ReleasePool():Timer{}
+    
+    ReleasePool(size_t delPoolSize) : Timer{}
     {
-        // nothing
+        deletionPool.reserve(delPoolSize);
+        startTimer(2000); // two seconds
     }
     
     void add(Ptr ptr)
     {
-        // add
+        auto messManager = juce::MessageManager::getInstanceWithoutCreating();
+        if (messManager && messManger.isThisMessageThread())
+        {
+            // message thread, add to pool directly
+            addIfNotAlreadyThere(ptr);
+        }
+        else
+        {
+            // on audio thread, push to fifo to deal with later.
+            newAddition = holdPool.push(ptr);
+            jassert(newAddition);
+        }
     }
     
     void add (Array array)
     {
-        //add the individual elements
+        //TODO loop through array andd add each element
+        
     }
     
     void timerCallback() override
     {
+       if (newAddition.compareAndSetBool (false, true))
+       {
+           Ptr object;
+           while (holdPool.getNumAvailableForReading() >0)
+           {
+               paramFifo.exchange(object);
+               addIfNotAlreadyThere(object);
+               object = nullptr; // ready for next one!
+           }
+       }
+       // Ok time to clean the pool.
+        deletionPool.erase(std::remove_if(deletionPool.begin(), deletionPool.end(), readyToDelete), deletionPool.end());
         
     }
     
@@ -39,6 +66,16 @@ private:
     
     void addIfNotAlreadyThere(Ptr ptr)
     {
-    
+      //TODO use std::find to check if ptr is in the pool. if it is not, then pushback
     }
+    
+    bool readyToDelete(const Ptr& ptr)
+    {
+        return ptr.get()->getReferenceCount() <= 1;
+    }
+    
+    std::vector<Ptr> deletionPool;
+    Fifo<Ptr, PoolSize> holdPool;
+    Atomic<bool> newAddition {false};
+    
 };
