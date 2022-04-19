a77344510dfd5be1ca67175e3a2cde1685a27ff1
Dr Entropy <rjljr2@gmail.com>
2022-04-19 15:35:30 -0700

    Slowing working through FilterLink Imp PFM 11-13

--- Source/FilterLink.h
+++ Source/FilterLink.h
@@ -13,35 +13,105 @@
 #include "FilterCoefficientGenerator.h"
 #include "ReleasePool.h"
 #include "Fifo.h"
+#include "Decibel.h"
+#include "CoeffTypeHelpers.h"
+
+
+
+
 
 // FifoDataType is for example ReferenceCountedObjectPtr
 // ParamType is one of the FilterParameters types.
 // Function type is going to be CoefficientsMaker for now.
 // FilterType is juce::dsp::IIR::Filter<float> or a chain of them.
 
-template <typename FilterType, FifoDataType, ParamType, FunctionType>
+template <typename FilterType, typename FifoDataType,  typename ParamType, typename FunctionType>
 struct FilterLink
 {
-    void prepare(const dsp::ProcessSpec&);
-    void reset();
+    void prepare(const juce::dsp::ProcessSpec& spec)
+    {
+        filter.prepare(spec);
+        
+        // update sampleRate
+        sampleRate = spec.sampleRate;
+        
+    }
+    
+    void reset()
+    {
+        filter.reset();
+    }
     
     template<typename ContextType>
-    void process(const ContextType& context);
+    void process(const ContextType& context)
+    {
+        if(! currentParams.bypassed)
+           filter.process(context);
+    }
     
     //stuff for the juce::SmoothedValue instances.
-    void updateSmootherTargets();
-    void resetSmoothers(float rampTime);
-    bool isSmoothing() const;
-    void checkIfStillSmoothing();
-    void advanceSmoothers(int numSamples);
+    // commented out for now, will come back to when i get the rest working.
+//    void updateSmootherTargets();
+//    void resetSmoothers(float rampTime);
+//    bool isSmoothing() const;
+//    void checkIfStillSmoothing();
+//    void advanceSmoothers(int numSamples);
     
     //stuff for updating the params
-    void updateParams(const ParamType& params);
+    void updateParams(const ParamType& params, bool forceUpdate = false)
+    {
+       if(forceUpdate || params != currentParams)
+       {
+           //coeffGen.changeParameters(params);
+           shouldComputeNewCoefficients = true;
+       }
+    }
+
     
     //stuff for updating the coefficients from processBlock, prepareToPlay, or setStateInformation
-    void updateCoefficients(const FifoDataType& coefficents);
-    void loadCoefficients(bool fromFifo);
-    void generateNewCoefficientsIfNeeded();
+    void updateCoefficients(const FifoDataType& coefficents)
+    {
+        if constexpr( isReferenceCountedObjectPtr<FifoDataType>::value )
+        {
+            updateFilterState(filter.coefficients, coefficents);
+        }
+        else if constexpr ( isReferenceCountedArray<FifoDataType>::value )
+        {
+            configureCutFilterChain(coefficents);
+        }
+        else
+        {
+            jassertfalse;  // this should not happen
+        }
+    }
+    
+    
+    void loadCoefficients(bool fromFifo)
+    {
+        if(fromFifo)
+        {
+            FifoDataType newCoefficients;
+            while(coeffFifo.pull(newCoefficients))
+            {
+                updateCoefficients(newCoefficients);
+            }
+        }
+        else
+        {
+            updateCoefficients(FunctionType::makeCoefficients(currentParams));
+        }
+    }
+    void generateNewCoefficientsIfNeeded()
+    {
+        if(shouldComputeNewCoefficients.compareAndSetBool(false,true))
+        {
+            ParamType newParams {currentParams};
+            // TODO update newParams using smoothed values for freq/gain/quality..
+            // this will probably require some more type checking to see if it has a gain parameter or not.
+            coeffGen.add(newParams);
+            
+        }
+    }
     
     //stuff for configuring the filter before processing
     void performPreloopUpdate(const ParamType& params);
@@ -49,8 +119,38 @@ struct FilterLink
     void initialize(const ParamType& params, float rampTime, bool onRealTimeThread, double sr);
 private:
     //stuff for setting the coefficients of the FilterType instance.
-    updateFilterState(Ptr& oldState, Ptr newState);
-    void configureCutFilterChain(const FifoDataType& coefficients);
+
+    void updateFilterState(FifoDataType& oldState, FifoDataType newState)
+    {
+        *oldState = *newState;
+        releasePool.add(newState);
+    }
+    
+    void configureCutFilterChain(const FifoDataType& coefficients)
+    {
+        // assumes CutFilter has 4 filters.
+        filter.template setBypassed<0>(true);
+        filter.template setBypassed<1>(true);
+        filter.template setBypassed<2>(true);
+        filter.template setBypassed<3>(true);
+        
+        int order {coefficients.size()};
+        switch(order)
+        {
+            case 4:
+                updateFilterState(filter.template get<3>().coefficients, coefficients[3]);
+                filter.template setBypassed<3>(false);
+            case 3:
+                updateFilterState(filter.template get<2>().coefficients, coefficients[2]);
+                filter.template setBypassed<2>(false);
+            case 2:
+                updateFilterState(filter.template get<1>().coefficients, coefficients[1]);
+                filter.template setBypassed<1>(false);
+            case 1:
+                updateFilterState(filter.template get<0>().coefficients, coefficients[0]);
+                filter.template setBypassed<0>(false);
+        }
+    }
     
     // magic numebrs
     static const int fifoSize = 100;
@@ -63,12 +163,16 @@ private:
     
     ParamType currentParams;
     
-    bool shouldComputeNewCoefficients;
+    juce::Atomic <bool> shouldComputeNewCoefficients{true};
  
     ReleasePool<Coefficients, poolSize> releasePool {poolSize, cleanupInterval};
     Fifo <FifoDataType, fifoSize>  coeffFifo;
     FilterCoefficientGenerator<FifoDataType, ParamType, CoefficientsMaker, fifoSize> coeffGen {coeffFifo};
-    FilterType myFilter;
+    FilterType filter;
     
+    // Smoothers
+//    juce::SmoothedValue<float> freqSmoother;
+//    juce::SmoothedValue<float> qualitySmoother;
+//    juce::SmoothedValue<Decibel<float>> gainSmoother;
     
 };
