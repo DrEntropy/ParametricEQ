2c8bcc6486df5701ba905a1dceeb1ef636b31b8b
Dr Entropy <rjljr2@gmail.com>
2022-04-28 15:19:15 -0700

    Draw image PFM11-20

--- Source/DbScale.cpp
+++ Source/DbScale.cpp
@@ -16,30 +16,19 @@
 
 void DbScale::paint (juce::Graphics& g)
 {
-    /* This demo code just fills the component's background and
-       draws some placeholder text to get you started.
-
-       You should replace everything in this method with your own
-       drawing code..
-    */
-
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
-
-    g.setColour (juce::Colours::grey);
-    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-
-    g.setColour (juce::Colours::white);
-    g.setFont (14.0f);
-    g.drawText ("DbScale", getLocalBounds(),
-                juce::Justification::centred, true);   // draw some placeholder text
+    g.drawImage(bkgd, getLocalBounds().toFloat());
 }
  
 void DbScale::buildBackgroundImage(int dbDivision, juce::Rectangle<int> meterBounds, int minDb, int maxDb)
 {
+    jassert(minDb <= maxDb);
+    auto bounds = getLocalBounds();
+    if(bounds.isEmpty())
+        return;
     
 }
 
-static std::vector<Tick> DbScale::getTicks(int dbDivision, juce::Rectangle<int> meterBounds, int minDb, int maxDb)
+std::vector<Tick> DbScale::getTicks(int dbDivision, juce::Rectangle<int> meterBounds, int minDb, int maxDb)
 {
     
 }
