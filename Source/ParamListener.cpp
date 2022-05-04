3d9e68f2c9c58999d189e2a37a78c24d43227c93
Dr Entropy <rjljr2@gmail.com>
2022-05-04 09:46:29 -0700

    Initial version of ParamListener PFM11-25

---
+++ Source/ParamListener.cpp
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    ParamListener.cpp
+    Created: 4 May 2022 9:34:45am
+    Author:  Ronald Legere
+
+  ==============================================================================
+*/
+
+#include "ParamListener.h"
+
+
+ParamListener::ParamListener(juce::RangedAudioParameter* parameter, std::function<void (float)> lambda) : audioParameter{ parameter }, listener {lambda}
+{
+    jassert(parameter);
+    jassert(lambda);
+    
+    cachedValue = audioParameter->getValue();
+    
+    startTimerHz(POLLING_FREQ);
+}
+
+
+void ParamListener::timerCallback()
+{
+    float value = audioParameter->getValue();
+    
+    if(value != cachedValue)
+    {
+        cachedValue = value;
+        listener(audioParameter->convertFrom0to1(value));
+    }
+
+}
