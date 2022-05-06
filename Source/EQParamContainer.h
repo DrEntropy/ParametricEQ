ca99df1c7508072da798a6309b98bcbe760770f2
Dr Entropy <rjljr2@gmail.com>
2022-05-06 13:19:40 -0700

    Added all filters to container PFM11-27

--- Source/EQParamContainer.h
+++ Source/EQParamContainer.h
@@ -22,6 +22,13 @@ public:
     EQParamContainer(juce::AudioProcessorValueTreeState& apvts) : apvts(apvts)
     {
         addAndMakeVisible(lowCut);
+        addAndMakeVisible(param1);
+        addAndMakeVisible(param2);
+        addAndMakeVisible(param3);
+        addAndMakeVisible(param4);
+        addAndMakeVisible(param5);
+        addAndMakeVisible(param6);
+        addAndMakeVisible(highCut);
 
     }
 
@@ -41,12 +48,28 @@ public:
         auto bounds = getLocalBounds();
         auto width = bounds.getWidth();
         lowCut.setBounds(bounds.removeFromLeft(width/8));
+        param1.setBounds(bounds.removeFromLeft(width/8));
+        param2.setBounds(bounds.removeFromLeft(width/8));
+        param3.setBounds(bounds.removeFromLeft(width/8));
+        param4.setBounds(bounds.removeFromLeft(width/8));
+        param5.setBounds(bounds.removeFromLeft(width/8));
+        param6.setBounds(bounds.removeFromLeft(width/8));
+        highCut.setBounds(bounds);
+        
     }
 
 private:
     
     juce::AudioProcessorValueTreeState& apvts;
     EQParamWidget lowCut {apvts, 0, true};
+    EQParamWidget param1 {apvts, 1, false};
+    EQParamWidget param2 {apvts, 2, false};
+    EQParamWidget param3 {apvts, 3, false};
+    EQParamWidget param4 {apvts, 4, false};
+    EQParamWidget param5 {apvts, 5, false};
+    EQParamWidget param6 {apvts, 6, false};
+    EQParamWidget highCut {apvts, 7, true};
+    
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EQParamContainer)
 };
