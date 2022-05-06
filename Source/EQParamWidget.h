e1a2780decac253c8b7c56db86637e42e1aaff92
Dr Entropy <rjljr2@gmail.com>
2022-05-06 15:36:03 -0700

    Barebones working widgets! PFM11-26

--- Source/EQParamWidget.h
+++ Source/EQParamWidget.h
@@ -13,8 +13,11 @@
 #include <JuceHeader.h>
 #include "ParameterHelpers.h"
 #include "ParamListener.h"
+#include "FilterInfo.h"
 
- 
+#define SLIDER_BORDER 2
+#define BUTTON_MARGIN 3
+#define BUTTONS_SIDE_MARGIN 5
 
 
 
@@ -37,8 +40,17 @@ struct HertzSlider : TextOnlyHorizontalSlider
 {
     juce::String getDisplayString()
     {
+        bool addK {false};
         auto freq = getValue();
-        return std::to_string(freq) + " Hz";
+        
+        if(freq >= 1000.0f)
+        {
+            freq /= 1000.0f;
+            addK = true;
+        }
+        
+        auto freqString = juce::String(freq, (addK ? 2 : 0));
+        return freqString + (addK ? "kHz" : "Hz");
     }
 };
 
@@ -46,7 +58,7 @@ struct QualitySlider : TextOnlyHorizontalSlider
 {
     juce::String getDisplayString()
     {
-        return "";
+        return juce::String(getValue(), 2);
     }
 };
 
@@ -54,7 +66,8 @@ struct SlopeSlider : TextOnlyHorizontalSlider
 {
     juce::String getDisplayString()
     {
-        return " dB/oct";
+        auto slope = static_cast<FilterInfo::Slope>(getValue());
+        return FilterInfo::mapSlopeToString.at(slope);
     }
 };
 
@@ -62,7 +75,7 @@ struct GainSlider : TextOnlyHorizontalSlider
 {
     juce::String getDisplayString()
     {
-        return " dB";
+        return juce::String(getValue(), 2) + " dB";
     }
 };
 
@@ -73,27 +86,42 @@ struct EQParamLookAndFeel : juce::LookAndFeel_V4
                                               float sliderPos, float minSliderPos, float maxSliderPos,
                                               const juce::Slider::SliderStyle style, juce::Slider& slider) override
     {
-        // only override LinearHorizontal (but why? This is bar already. Also this doesnt work because the drawLinearSlider call for LinearHorizontal adds
-        // some padding for some reason.
-        
-        if(TextOnlyHorizontalSlider* textOnlyHSliderp = dynamic_cast<TextOnlyHorizontalSlider *> (&slider) )
+        if(TextOnlyHorizontalSlider* textOnlyHSliderp = dynamic_cast<TextOnlyHorizontalSlider*> (&slider))
         {
             auto bounds = slider.getLocalBounds().toFloat();
-            // rescale slider positions
-            sliderPos = juce::jmap(sliderPos, static_cast<float> (x), static_cast<float> (x) + width, 0.f, bounds.getWidth());
-            g.setColour (juce::Colours::black);
-            auto newBounds =juce::Rectangle<float> (0.f, 0.f, sliderPos,  bounds.getHeight());
-            g.fillRect (newBounds);
-            g.setColour (juce::Colours::white);
-            juce::String message =textOnlyHSliderp->getDisplayString();
-            g.drawFittedText(message,bounds.toNearestInt(), juce::Justification::centred, 1);
+
+            g.setColour(juce::Colours::white);
+            g.drawRect(bounds, SLIDER_BORDER);
+            bounds.reduce(SLIDER_BORDER, SLIDER_BORDER);
+            
+            // rescale slider position
+            sliderPos = juce::jmap(sliderPos, static_cast<float>(x), static_cast<float>(x) + width, bounds.getX(), bounds.getWidth());
+            
+            g.setColour(juce::Colours::black);
+            auto barBounds = bounds.withWidth(sliderPos);
+            g.fillRect(barBounds);
+            
+            g.setColour(juce::Colours::white);
+            juce::String message = textOnlyHSliderp->getDisplayString();
+            g.drawFittedText(message, bounds.toNearestInt(), juce::Justification::centred, 1);
         }
         else
         {
-            juce::LookAndFeel_V4::drawLinearSlider(g, x, y, width, height, sliderPos, minSliderPos, maxSliderPos,
-                                                           style, slider);
+            juce::LookAndFeel_V4::drawLinearSlider(g, x, y, width, height, sliderPos, minSliderPos, maxSliderPos, style, slider);
         }
- 
+    }
+    
+    void drawButtonBackground (juce::Graphics& g, juce::Button& button, const juce::Colour& backgroundColour, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+    {
+        auto bounds = button.getLocalBounds();
+        if(button.getToggleState())
+            g.setColour(juce::Colours::green);
+        else
+            g.setColour(juce::Colours::black);
+        
+        g.fillRect(bounds);
+        g.setColour(juce::Colours::white);
+        g.drawRect(bounds, 1);
         
     }
     
@@ -117,25 +145,25 @@ public:
         
         addAndMakeVisible(*gainOrSlopeSlider);
         
-        AttachSliders(Channel::Left);
+        attachSliders(Channel::Left);
         
-        addChildComponent(leftMidButton);
-        addChildComponent(rightSideButton);
-    
+        setUpButton(leftMidButton);
+        setUpButton(rightSideButton);
+        leftMidButton.setToggleState(true, juce::NotificationType::dontSendNotification);
         
         auto safePtr = juce::Component::SafePointer<EQParamWidget>(this);
         leftMidButton.onClick = [safePtr]()
         {
             if(auto* comp = safePtr.getComponent() )
-                comp->AttachSliders(Channel::Left);
+                comp->attachSliders(Channel::Left);
         };
         rightSideButton.onClick = [safePtr]()
         {
             if(auto* comp = safePtr.getComponent() )
-                comp->AttachSliders(Channel::Right);
+                comp->attachSliders(Channel::Right);
         };
         
-        
+ 
         ChannelMode mode = static_cast<ChannelMode>(apvts.getRawParameterValue("Processing Mode")->load());
         setProcessingMode(mode);
         
@@ -145,8 +173,6 @@ public:
                                               if(auto* comp = safePtr.getComponent() )
                                                   comp->setProcessingMode(static_cast<ChannelMode>(v));
                                              }));
-        
-
     }
     
 
@@ -160,7 +186,9 @@ public:
         if(mode != ChannelMode::DualMono)
         {
             leftMidButton.setVisible(true);
+            leftMidButton.setButtonText(mode == ChannelMode::Stereo ? "L" : "M");
             rightSideButton.setVisible(true);
+            rightSideButton.setButtonText(mode == ChannelMode::Stereo ? "R" : "S");
         }
         else
         {
@@ -168,8 +196,15 @@ public:
             rightSideButton.setVisible(false);
         }
     }
+    
+    void setUpButton(juce::Button& button)
+    {
+        addChildComponent(button);
+        button.setRadioGroupId(1);
+        button.setClickingTogglesState(true);
+    }
    
-    void AttachSliders(Channel channel)
+    void attachSliders(Channel channel)
     {
         frequencyAttachment.reset(); //must first delete old attachment before creating new one!
         frequencyAttachment.reset(new SliderAttachment(apvts, createFreqParamString(channel, filterNumber), frequencySlider));
@@ -190,24 +225,21 @@ public:
 
     void paint (juce::Graphics& g) override
     {
-
         g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
-        g.setColour(juce::Colours::blue);
-        g.drawRect(getLocalBounds(), 2);
     }
 
     void resized() override
     {
         auto bounds = getLocalBounds();
-        bounds.reduce(2,2);
         auto height = bounds.getHeight();
-        auto width = bounds.getWidth();
+       
         frequencySlider.setBounds(bounds.removeFromTop(height/4));
         qSlider.setBounds(bounds.removeFromTop(height/4));
         gainOrSlopeSlider->setBounds(bounds.removeFromTop(height/4));
-        leftMidButton.setBounds(bounds.removeFromLeft(width/2));
-        rightSideButton.setBounds(bounds);
- 
+        
+        bounds.reduce(BUTTONS_SIDE_MARGIN, 0);
+        leftMidButton.setBounds(bounds.removeFromLeft(bounds.getWidth()/2).reduced(BUTTON_MARGIN));
+        rightSideButton.setBounds(bounds.reduced(BUTTON_MARGIN));
     }
     
 
@@ -222,8 +254,9 @@ private:
     std::unique_ptr<juce::Slider> gainOrSlopeSlider;
     juce::TextButton leftMidButton, rightSideButton;
     
-    // TODO add ParamListeneres for  bypass
     std::unique_ptr<ParamListener> modeListener;
+    std::unique_ptr<ParamListener> leftMidBypassListener;
+    std::unique_ptr<ParamListener> rightSideBypassListener;
     
     using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     
