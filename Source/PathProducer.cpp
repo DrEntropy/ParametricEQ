62b8276a2b6f0917dd0499d4a7523b3d328d5161
Dr Entropy <rjljr2@gmail.com>
2022-05-16 10:12:57 -0700

    Skeleton for PathProducer PFM11-33

--- Source/PathProducer.cpp
+++ Source/PathProducer.cpp
@@ -9,3 +9,82 @@
 */
 
 #include "PathProducer.h"
+
+template<typename BlockType>
+PathProducer<BlockType>::PathProducer(double sr, SingleChannelSampleFifo<BlockType>&)
+{
+    // do stuff
+}
+
+
+template<typename BlockType>
+PathProducer<BlockType>::~PathProducer()
+{
+    //
+}
+
+template<typename BlockType>
+void PathProducer<BlockType>::run()
+{
+    
+}
+
+template<typename BlockType>
+void PathProducer<BlockType>::changeOrder(FFTOrder o)
+{
+    
+}
+
+template<typename BlockType>
+int PathProducer<BlockType>::getFFTSize() const
+{
+    
+}
+
+template<typename BlockType>
+double PathProducer<BlockType>::getBinWidth() const
+{
+    
+}
+
+template<typename BlockType>
+void PathProducer<BlockType>::pauseThread()
+{
+    
+}
+
+template<typename BlockType>
+void PathProducer<BlockType>::setFFTRectBounds(juce::Rectangle<float>)
+{
+    
+}
+
+template<typename BlockType>
+void PathProducer<BlockType>::setDecayRate(float dr)
+{
+    
+}
+
+template<typename BlockType>
+bool PathProducer<BlockType>::pull(juce::Path&&)
+{
+    
+}
+
+template<typename BlockType>
+int PathProducer<BlockType>::getNumAvailableForReading() const
+{
+    
+}
+
+template<typename BlockType>
+void PathProducer<BlockType>::toggleProcessing(bool)
+{
+    
+}
+
+template<typename BlockType>
+void PathProducer<BlockType>::changePathRange(float negativeInfinityDb, float maxDb)
+{
+    
+}
